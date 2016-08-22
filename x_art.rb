a49491dfe68bfc4ac72a4aec102a23dd1d9d496f
Jake Song <jake@xlgames.com>
2016-08-22 09:24:11 +0900

    작동

--- x_art.rb
+++ x_art.rb
@@ -3,11 +3,13 @@ require 'watir'
 
 # XArt scraper
 class XArt
-  attr_accessor :browser
+  attr_accessor :browser, :wait_time, :y_offset
 
   Info = Struct.new(:title, :date, :image, :skip)
 
   def initialize
+    @wait_time = 10
+    @y_offset = 0
     switches = %w(--user-data-dir=/Users/apple/Library/Application\ Support/Google/Chrome/)
     @browser = Watir::Browser.new :chrome, switches: switches
     @browser.goto 'http://www.x-art.com/updates/'
@@ -15,10 +17,10 @@ class XArt
 
   def wait_element(*args)
     start_time = Time.now
-    while Time.now - start_time < 5
+    while Time.now - start_time < @wait_time
       el = @browser.element(*args)
+      sleep(0.01)
       return el if el.exist?
-      sleep(0.1)
     end
     nil
   end
@@ -36,12 +38,20 @@ class XArt
     strs2 = strs[0].split(' ')
     month_str = strs2[0]
     day = strs2[1].to_i
-    month = %w(JAN FEB MAR APR MAY JUN JUL AUG OCT NOV DEC).index(month_str) + 1
+    month_index = %w(JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC).index(month_str)
+    if month_index.nil?
+      puts "Invalid month #{month_str}"
+      month = 1
+    else
+      month = month_index + 1
+    end
     format '%04d-%02d-%02d', year, month, day
   end
 
   # div 에서 각종 정보를 찾는다
   def find_info(div)
+    return nil if div.wd.location.y < @y_offset
+
     header = div.div(class: 'item-header')
     header = div.div(class: 'columns') unless header.exist?
     return nil unless header.exist?
@@ -61,8 +71,12 @@ class XArt
   end
 
   def same_title_adjacent?(infos, i)
-    i > 0 && infos[i - 1].title == infos[i].title ||
-      i < infos.length - 1 && infos[i + 1].title == infos[i].title
+    (i - 1..0).each do |j|
+      return true if infos[j].title == infos[i].title
+    end
+    (i + 1..infos.length - 1).each do |j|
+      return true if infos[j].title == infos[i].title
+    end
   end
 
   def mark_duplicate(infos)
@@ -79,7 +93,9 @@ class XArt
       last_index = infos.find_index do |info|
         info.title == old_infos.last.title && info.image == old_infos.last.image
       end
-      infos = infos[last_index + 1, infos.length]
+      if last_index
+        infos = infos[last_index + 1, infos.length]
+      end
     end
     infos = mark_duplicate(infos)
     infos.select { |info| !info.skip }
@@ -114,17 +130,23 @@ class XArt
   def enter(info)
     quick_search = wait_element(tag_name: 'input',
                                 class: 'QuickSearchInput-searchInput-wnRxj')
-    quick_search.to_subtype.set info.title
+    quick_search.to_subtype.set 'x-art ' + info.title
 
     candidate = wait_element(class: 'QuickSearchResult-button-xsMFe')
     return if candidate.nil?
     candidate.click
 
     edit_button = wait_element(class: 'edit-btn')
+    return if edit_button.nil?
     edit_button.click
 
     input_field_title('lockable-title', '[X-Art] ' + info.title)
     input_field_title('lockable-titleSort', '[X-Art] ' + info.title)
+
+    if info.date == '2015-09-31'
+      info.date = '2015-09-30'
+    end
+
     input_field_date('lockable-originallyAvailableAt', info.date)
     input_field_title('lockable-contentRating', 'X')
     input_field_title('lockable-studio', 'X-Art')
@@ -137,10 +159,14 @@ class XArt
 
     input_field_url('url', info.image)
 
+    @wait_time = 10
     wait_element_visible(class: 'upload-back-btn')
 
+    @wait_time = 3
     save_button = wait_element(class: 'save-btn')
     save_button.click
+
+    sleep 0.1 while save_button.exist? && save_button.visible?
   end
 
   def print(infos)
@@ -149,27 +175,53 @@ class XArt
     end
   end
 
+  def last_item_info
+    infos = find_infos
+    infos.last
+    # items = @browser.divs(class: 'item')
+    # (items.length - 1..0).each do |item|
+    #   info = find_info(item)
+    #   return info if !info.nil?
+    # end
+  end
+
+  def next_page
+    # info = last_item_info
+    # puts info.title
+
+    @browser.send_keys(:page_down)
+    sleep 3
+    @y_offset = @browser.execute_script 'return window.pageYOffset;'
+
+    # while last_item_info == info do
+    #   puts last_item_info.title
+    #   sleep 0.5
+    # end
+  end
+
   def scrape
     old_infos = nil
-    loop do
+    empty_count = 0
+    while empty_count < 3
       infos = find_unique_infos(old_infos)
 
       open_plex
-      infos.each do |info|
-        enter(info)
+
+      if infos.empty?
+        empty_count += 1
+      else
+        empty_count = 0
+
+        infos.each do |info|
+          enter(info)
+        end
+        @browser.windows.last.close
+
+        old_infos = infos
       end
 
-      @browser.windows.last.close
-
-      item_count = @browser.divs(class: 'item').length
-      @browser.send_keys :page_down
-
-      sleep 0.1 while item_count == @browser.divs(class: 'item').length
-
-      puts "#{item_count} #{@browser.divs(class: 'item').length}"
+      next_page
       puts '------------------------------'
-
-      old_infos = infos
     end
   end
 end
